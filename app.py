59cba4568631f13cfebf394159e93e881af40b79
Bentani Amin <78359840+bentaniamin1@users.noreply.github.com>
2025-02-02 23:01:39 +0100

    Fonc : Create metrics in app, and event to upda

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
+from prometheus_client import generate_latest, CollectorRegistry, Counter, Gauge
+import psutil
+
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+REQUEST_COUNTER = Counter('http_requests_total', 'Total HTTP Requests')
+CPU_USAGE = Gauge('cpu_usage_percent', 'CPU Usage in Percent')
+
+
+
+def update_cpu_usage():
+    cpu_percent = psutil.cpu_percent()
+    CPU_USAGE.set(cpu_percent)
+   
 @app.route('/metrics', methods=['GET'])
 def metric():
-    # # Example metric data
-    # data = {
-    #     'cpu_usage': '70%',
-    #     'memory_usage': '65%'
-    # }
-    # return jsonify(data)
+    update_cpu_usage()
+    return generate_latest()
 
-@app.route('/healthy', methods=['GET'])
+@app.route('/health', methods=['GET'])
 def healthy():
     return jsonify({'status': 'healthy OK'})
+
+@app.route('/')
+def home():
+    REQUEST_COUNTER.inc()  
+    return "Hello, Prometheus!"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
